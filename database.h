988a07aadc4016745e9a75b4e4b888c843f3edf2
syarin03 <gami5507@gmail.com>
2023-02-20 21:16:44 +0900

    commit

---
+++ database.h
@@ -0,0 +1,10 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QSqlDatabase>
+#include <QSqlDriver>
+#include <QsqlError>
+#include <QDebug>
+#include <QSqlTableModel>
+
+#endif // DATABASE_H
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "database.h"
+#include <QApplication>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "test" << endl;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("10.10.21.116");
+    db.setDatabaseName("starbocks");
+    db.setUserName("starbocks_admin");
+    db.setPassword("admin1234");
+    bool ok = db.open();
+    cout << ok << endl;
+    QSqlQuery query;
+    QString sql;
+    sql = "SELECT * FROM member";
+    query.exec(sql);
+    while (query.next())
+    {
+        int num = query.value(0).toInt();
+    }
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
